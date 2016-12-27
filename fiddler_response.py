db6df61772ea5aee49d0aefec16e78815a95f7b9
wenkai.ding <734756080@qq.com>
2016-12-27 14:16:46 +0800

    add check
    check ui and fidder response

--- fiddler_response.py
+++ fiddler_response.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
+import json
 
 class fiddler_response:
     def __init__(self):
@@ -10,15 +11,30 @@ class fiddler_response:
         content_bh = f_bh.read().decode("utf-16")
         lines_bh = content_bh.split('\n')
         tmp = lines_bh[4].split("data\":")
-        print tmp[1]
-        item = tmp[1].replace('u\'','\'r')
-        print item
-        return item[0].split(", \"info\"")
+        tem = tmp[1].split("\"info\"")
+        return  json.loads(str(tem[0][0:tem[0].__len__()-1]))
 
     def get_response_list(self):
-        pass
+        item = []
+        for tem in self.get_file()["product_list"]:
+            item.append(tem)
+        return item
+
+    def check_response(self,ui,fid):
+        if isinstance(ui,list) and isinstance(fid,list):
+            for i in range(0,ui.__len__()):
+                self.check_response(ui[i],fid[i])
+
+        elif isinstance(ui,dict) and isinstance(fid,dict):
+            for k,v in ui:
+                assert ui[k]==fid[k],"ui result is different from fiddler response，" \
+                                     "ui result is：{} and fiddler response is ：{}".format(ui[k],fid[k])
+        else:
+            return "input error，must be list or dict"
+
+
+
 if __name__ == '__main__':
     a= fiddler_response()
-    print type(a.get_file()[0].replace('u\'',''))
-    print a.get_file()[0].replace('u\'',' ')
-    print a.get_file()
+
+    print a.get_response_list()[0]["pic"]
